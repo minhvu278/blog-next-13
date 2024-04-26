853137c23119c913538573681cdb04b65146314f
minhvu278 <dominhvu278@gmail.com>
2024-04-27 01:19:17 +0700

    Add navigation and logo

---
+++ components/layout/logo.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import classes from './logo.module.css'
+
+function Logo(props) {
+    return (
+        <div className={classes.logo}>minhvu278</div>
+    );
+}
+
+export default Logo;
