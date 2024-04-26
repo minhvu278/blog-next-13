b1f35bdd88fbee0e45eb255e73c7daffab7228ff
minhvu278 <dominhvu278@gmail.com>
2024-04-27 01:32:16 +0700

    Add layout grid

---
+++ components/home-page/featured-posts.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import classes from './featured-posts.module.css'
+
+function FeaturedPosts(props) {
+    return (
+        <section className={classes.latest}>
+            <h2>Featured Poests</h2>
+        </section>
+    );
+}
+
+export default FeaturedPosts;
---
+++ components/posts/post-item.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function PostItem(props) {
+    return (
+        <div></div>
+    );
+}
+
+export default PostItem;
