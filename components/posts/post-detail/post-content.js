409efb77b700254f418d8825f2566646a2bec870
minhvu278 <dominhvu278@gmail.com>
2024-05-01 23:13:47 +0700

    Render custom html element with react markdown

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -3,20 +3,13 @@ import classes from './post-content.module.css'
 import PostHeader from "./post-header";
 import ReactMarkdown from "react-markdown"
 
-const DUMMY_POSTS = {
-    slug: "getting-started",
-    title: "Getting Started",
-    image: "getting-started.png",
-    date: "2024-02-10",
-    content: `# This is first post`
-}
-
 function PostContent(props) {
-    const imagePath = `/images/posts/${DUMMY_POSTS.slug}/${DUMMY_POSTS.image}`
+    const {post} = props
+    const imagePath = `/images/posts/${post.slug}/${post.image}`
     return (
         <article className={classes.content}>
-            <PostHeader title={DUMMY_POSTS.title} image={imagePath} />
-            <ReactMarkdown>{DUMMY_POSTS.content}</ReactMarkdown>
+            <PostHeader title={post.title} image={imagePath} />
+            <ReactMarkdown>{post.content}</ReactMarkdown>
         </article>
     );
 }
